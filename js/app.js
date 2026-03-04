71809f1ae16f5e770e53a0db0ea32580529b5f70
Andres Moncayo <andresfelipemoncayopaez@gmail.com>
2026-03-03 23:43:40 -0500

    Implementacion-Scss-Variables-Tema-Dark-Light

--- js/app.js
+++ js/app.js
@@ -1,75 +1,163 @@
-// app.js
 
+
+
+
+// 1. URL de la API (Fake Store API)
 const API_URL = 'https://fakestoreapi.com/products?limit=8';
-let allProducts = [];
+let AllProducts = [];
+let Allfiltered = []; 
 
-// 1. Obtener datos
+// 2. Función para obtener los datos
 async function getProducts() {
     try {
         const response = await fetch(API_URL);
-        allProducts = await response.json();
-        renderProducts(allProducts); // Pintar inicial
-        setupFilters();             // Activar botones
+        AllProducts = await response.json();
+
+        Allfiltered = AllProducts;
+        renderProducts(AllProducts);
+        setupFilters();
     } catch (error) {
         console.error("Error cargando productos:", error);
     }
 }
 
-// 2. Configurar Filtros
-function setupFilters() {
-    // Nota el punto (.) porque es una clase
-    const filterButtons = document.querySelectorAll(".c-filter__btn");
 
-    filterButtons.forEach(btn => {
-        btn.addEventListener("click", () => {
-            // A. Quitar la clase active a TODOS los botones
-            filterButtons.forEach(b => b.classList.remove("c-filter__btn--active"));
-            
-            // B. Poner la clase active al botón que clickeamos
-            btn.classList.add('c-filter__btn--active');
 
-            // C. Filtrar
-            const category = btn.getAttribute("data-category");
 
-            if(category === "all"){
-                renderProducts(allProducts);
-            } else {
-                const filtered = allProducts.filter(p => p.category === category);
-                renderProducts(filtered);
-            }
-        });
-    });
+
+
+
+
+
+
+// Configuracion Theming
+
+const root = document.documentElement; // Defino Root como el Html
+const themeToggleBtn = document.getElementById("theme-toggle");
+
+function getCurrentTheme() { // Obtener el tema actual
+  return root.getAttribute('data-theme') || 'Auto';
 }
 
-// 3. Renderizar (Pintar en HTML)
+function setTheme(theme){  // Establecer la palabra del tema actual
+    root.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+    if (themeToggleBtn){
+        themeToggleBtn.textContent = `Tema: ${theme}`;
+    }
+}
+
+
+function applySavedTheme(){ // Guardar
+    const saved = localStorage.getItem('theme');
+    if (saved === 'Light' || saved === 'Dark' || saved === 'Auto'){
+        setTheme(saved);
+    }
+}
+
+
+if (themeToggleBtn) {
+  themeToggleBtn.addEventListener('click', () => {
+    const current = getCurrentTheme();
+    let next;
+    if (current === 'Auto') next = 'Dark';
+    else if (current === 'Dark') next = 'Light';
+    else next = 'Auto';
+
+    setTheme(next);
+    
+  });
+}
+
+applySavedTheme();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Ejercicio botones cambian de color al dar click y imprimir los de la categoria correspondiente
+function setupFilters(){
+  const filterButtons = document.querySelectorAll(".c-filter__btn[data-category]");
+
+  filterButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      // 1. Estado visual
+      filterButtons.forEach(b => b.classList.remove("c-filter__btn--active"));
+      btn.classList.add("c-filter__btn--active");
+
+      // 2. Filtrado
+      const category = btn.getAttribute("data-category");
+
+      if (category === "all") {
+        Allfiltered = AllProducts
+      } else {
+        Allfiltered = AllProducts.filter(p => p.category === category); // solo esa categoría
+      }
+
+      renderProducts(Allfiltered);
+    });
+  });
+}
+
+
+
+// 3. Función para pintar los productos en el HTML
 function renderProducts(products) {
+    
     const grid = document.getElementById('product-grid');
-    grid.innerHTML = ''; 
+    grid.innerHTML = ''; // Limpiar el contenedor
 
     products.forEach(product => {
-        
+
+        // Ejercicio,
+        // Lógica para cortar el título
+        // const shortTitle = product.title.length > 15 
+            //? product.title.substring(0, 15) + '...' 
+            //: product.title;
+
+
+        // Ejercicio, Si la categoria es Joyeria aplicar un estilo diferente
         let claseExtra = "";
 
-        // Si es joyería, asignamos la clase del borde dorado
         if (product.category === "jewelery"){
-            // ¡OJO AQUÍ! Agregué un espacio al inicio: ' c-card--featured'
-            // Sin ese espacio, quedaba 'c-cardc-card--featured' y se rompía.
-            claseExtra = ' c-card--featured';
+            claseExtra = 'c-card--featured';
         }
 
+
+        // Creamos la estructura usando tus clases de la capa COMPONENTS
         const productHTML = `
-            <article class="c-card${claseExtra}">
+            <div class=" c-card ${claseExtra}">
                 <img src="${product.image}" alt="${product.title}" class="c-card__image">
                 <div class="c-card__body">
                     <h2 class="c-card__title">${product.title}</h2>
                     <p class="c-card__price">$${product.price}</p>
-                    <button class="c-card__btn">Agregar al carrito</button>
+                    <button class="c-card__btn c-card__btn--buy">
+                        Agregar al carrito
+                    </button>
                 </div>
-            </article>
+            </div>
         `;
         grid.innerHTML += productHTML;
     });
 }
 
-// Iniciar
+// Ejecutar la función al cargar la página
 getProducts();
